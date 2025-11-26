ae8419dbf54513703c90e3a19efc407a36b7910f
이동주 <edcde74@gmail.com>
2025-11-26 11:21:00 +0900

    test_upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
-function App() {
-  return <div>hello</div>
+import React from 'react';
+
+class App extends React.Component {
+  state ={
+    count: 0,
+  };
+  render() {
+    return (
+      <div>
+        <h1>The Number is: {this.state.count}</h1>
+        <button onClick ={this.add}>Add </button>
+        <button onClick ={this.minus}>Minus</button>
+        </div>  
+    );
+  }
+
+  add = () => {
+    this.setState((current)=> ({
+      count: current.count + 1
+    }));
+  };
+
+  minus = () => {
+    this.setState((current)=> ({
+      count: current.count - 1
+    }));
+  };
 }
 
 export default App;
